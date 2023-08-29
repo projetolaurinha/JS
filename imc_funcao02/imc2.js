bc5b14ecb7475620efaff5c0939c856c59d96a26
Laura de Oliveira Sousa <100208612+projetolaurinha@users.noreply.github.com>
2023-08-29 11:08:56 -0300

    Add files via upload

---
+++ imc_funcao02/imc2.js
@@ -0,0 +1,31 @@
+function calcula_imc(event){
+
+    event.preventDefault();
+
+    var altura = document.imcform.altura.value;
+    var peso = document.imcform.peso.value;
+    var calculo = (peso/(altura*altura));
+
+    if(calculo< 18.5){
+        alert("você está magro com esse indice:  " + calculo);
+    }
+
+    else if(calculo>=18.5 && calculo< 24.9){
+        alert("você está normal com esse indice:  " + calculo);
+    }
+    
+    else if(calculo>=25 && calculo< 29.9){
+        alert("você está sobre peso com esse indice:  " + calculo);
+    }
+
+    else if(calculo>=30 && calculo< 39.9){
+        alert("você está obesidade com esse indice:  " + calculo);
+    }
+
+    else if(calculo>40){
+        alert("você está obesidade grave com esse indice:  " + calculo);
+    }
+
+    document.getElementById('resul').innerHTML= resultado;
+
+}
