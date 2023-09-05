fd4fd1418cbc7423fe93a8fcc8628155c81be277
Laura de Oliveira Sousa <100208612+projetolaurinha@users.noreply.github.com>
2023-09-05 10:49:33 -0300

    Add files via upload

---
+++ tabuada/tabuada.js
@@ -0,0 +1,20 @@
+function chama_tabuada(){
+
+var var1= document.tabform.var1.value
+
+var var2= document.tabform.var2.value
+
+
+  for(i=var1; var1<=var2; var1++){
+
+
+      for (j=1; j<=10; j++){
+
+          document.write(var1+'x' + j+ '='+ var1*j+ '<br>');
+        }
+
+     document.write('<br>')
+
+
+    }
+}
